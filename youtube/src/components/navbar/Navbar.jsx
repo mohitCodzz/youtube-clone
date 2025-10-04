c71d6908dedc232358febabbf78d3412cec5f3b9
Mohit <mohitt.tech@gmail.com>
2025-10-05 05:08:07 +0530

    Update Navbar.jsx

--- youtube/src/components/navbar/Navbar.jsx
+++ youtube/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import search_icon from "../../assets/search.png";
 import upload_icon from "../../assets/upload.png";
 import more_icon from "../../assets/more.png";
 import notifications_icon from "../../assets/notification.png";
-import user_icon from "../../assets/jack.png";
+import user_icon from "../../assets/user.png";
 
 const Navbar = ({ setSidebar, sidebar }) => {
   return (
