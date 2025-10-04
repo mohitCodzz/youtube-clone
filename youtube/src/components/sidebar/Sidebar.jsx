dca0551f018001ec84007fe3042823c3cd8e33ee
Mohit <mohitt.tech@gmail.com>
2025-10-05 01:45:03 +0530

    Update Sidebar.jsx

--- youtube/src/components/sidebar/Sidebar.jsx
+++ youtube/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import home from '../../assets/icons/home.png'
+import game_icon from '../../assets/icons/game.png'
+import automobiles from '../../assets/icons/automobiles.png'
+import sport from '../../assets/icons/sport.png'
+import entertainment from '../../assets/icons/entertainment.png'
+import tech from '../../assets/icons/tech.png'
+import music from '../../assets/icons/music.png'
+import blogs from '../../assets/icons/blogs.png'
+import news from '../../assets/icons/news.png'
+import jack from '../../assets/jack.png'
+import simon from '../../simon.png'
+import tom from '../../tom.png'
+import megan from '../../megan.png'
+import cameron from '../../cameron.png'
 
 const Sidebar = () => {
   return (
     <div>
-        
+
     </div>
   )
 }
