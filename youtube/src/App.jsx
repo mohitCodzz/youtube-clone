231275515f5eed69284e4ccb21da6eb146c1e262
Mohit <mohitt.tech@gmail.com>
2025-10-05 01:44:58 +0530

    Update App.jsx

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar' // Navbar component 
+import React from "react";
+import Navbar from "./components/navbar/Navbar"; // Navbar component
+import { Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-         <Navbar />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
+        
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
