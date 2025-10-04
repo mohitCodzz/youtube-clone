6bf3a7e276a4dd34e2fcc95812f04a08e2998c85
Mohit <mohitt.tech@gmail.com>
2025-10-05 04:57:40 +0530

    Update Feed.jsx

--- youtube/src/components/feed/Feed.jsx
+++ youtube/src/components/feed/Feed.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import thumbnail1 from '../../assets/thumbnail1.png';
+import thumbnail2 from '../../assets/thumbnail3.png';
+import thumbnail3 from '../../assets/thumbnail3.png';
+import thumbnail4 from '../../assets/thumbnail4.png';
+import thumbnail5 from '../../assets/thumbnail5.png';
+import thumbnail6 from '../../assets/thumbnail6.png';
+import thumbnail7 from '../../assets/thumbnail7.png';
+import thumbnail8 from '../../assets/thumbnail1.png';
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div>
+        <img src={thumbnail1} alt="thumbnail1" />
+        <h2>Most amazing view of the nature !</h2>
+    </div>
   )
 }
 
