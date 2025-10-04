c18ebf7491a22b66e2321b4d44365cbf23a05692
Mohit <mohitt.tech@gmail.com>
2025-10-05 04:52:34 +0530

    Update Home.jsx

--- youtube/src/pages/home/Home.jsx
+++ youtube/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
-
+import Feed from '../../components/feed/Feed'
 const Home = ({sidebar}) => {
   return (
     <>
     <Sidebar sidebar={sidebar }/>
+    <div className={`container ${sidebar ? "" : large-container}`}>
+     <Feed />
+    </div>
     </>
   )
 }
