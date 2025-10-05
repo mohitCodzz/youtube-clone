e864eff325585b465478eda45a25d7c9d9be76fe
Mohit <mohitt.tech@gmail.com>
2025-10-05 13:36:21 +0530

    Update Feed.jsx

--- youtube/src/components/feed/Feed.jsx
+++ youtube/src/components/feed/Feed.jsx
@@ -14,11 +14,11 @@ import thumbnail10 from '../../assets/thumbnail10.png';
 const Feed = () => {
   // Array of video data
   const videos = [
-    { thumbnail: thumbnail10, title: "Night Sky from ISRO HQ 🚀", channel: "SpaceX", views: "100M views", time: "1 day ago", videoId: "video10" },
+    { thumbnail: thumbnail10, title: "Night Sky from ISRO HQ 🚀", channel: "ISRO", views: "100M views", time: "1 day ago", videoId: "video10" },
     { thumbnail: thumbnail9, title: "SpaceX rocket launch 🚀", channel: "SpaceX", views: "100M views", time: "1 day ago", videoId: "video9" },
     { thumbnail: thumbnail1, title: "Most amazing view of the nature!", channel: "NatureXpo", views: "25M views", time: "1 week ago", videoId: "video1" },
     { thumbnail: thumbnail2, title: "USA 🇺🇸 !", channel: "WildLifeTV", views: "10M views", time: "3 days ago", videoId: "video2" },
-    { thumbnail: thumbnail3, title: "A day in New York", channel: "TechWorld", views: "5M views", time: "2 days ago", videoId: "video3" },
+    { thumbnail: thumbnail3, title: "A day in New York", channel: "TravelwithRiya", views: "5M views", time: "2 days ago", videoId: "video3" },
     { thumbnail: thumbnail4, title: "Top 10 travel destinations", channel: "TravelWithYash", views: "12M views", time: "1 day ago", videoId: "video4" },
     { thumbnail: thumbnail5, title: "Day out with friends", channel: "Guyzz", views: "8M views", time: "4 days ago", videoId: "video5" },
     { thumbnail: thumbnail6, title: "Lambo Unboxing 🎁", channel: "CarGuy", views: "15M views", time: "3 days ago", videoId: "video6" },
