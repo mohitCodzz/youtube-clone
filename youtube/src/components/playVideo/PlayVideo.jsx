f03e62d0400503a2dbd9b93b2a0a06e579599873
Mohit <mohitt.tech@gmail.com>
2025-10-05 16:09:02 +0530

    Update PlayVideo.jsx

--- youtube/src/components/playVideo/PlayVideo.jsx
+++ youtube/src/components/playVideo/PlayVideo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import tom from '../../assets/tom.png'
+import user from '../../assets/user.png'
+
 
 const PlayVideo = () => {
   return (
     <div>
-        
+        <video src={video1}></video>
     </div>
   )
 }
